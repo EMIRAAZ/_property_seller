0b0ab3cd541c3d02ec7281c1a349505622b051c7
nihad <mernnihadsoft@gmail.com>
2024-02-21 23:02:46 +0530

    again push

--- src/index.js
+++ src/index.js
@@ -13,14 +13,14 @@ import { SkeletonTheme } from "react-loading-skeleton";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <div className="text-white w-full h-screen bg-black">
-    <div className="w-[350px] h-[100vh] sm:flex sm:justify-center sm:justify-center sm:m-auto sm:w-[600px]">
-    <img className="object-contain w-full h-full" src="/assets/image/propertyselle_Logo.jpeg" />
-    </div>
-  </div>
-  // <Provider store={store()}>
-  //   <SkeletonTheme baseColor="#202020" highlightColor="#444">
-  //     <Router />
-  //   </SkeletonTheme>
-  // </Provider>
+  // <div className="text-white w-full h-screen bg-black">
+  //   <div className="w-[350px] h-[100vh] sm:flex sm:justify-center sm:justify-center sm:m-auto sm:w-[600px]">
+  //   <img className="object-contain w-full h-full" src="/assets/image/propertyselle_Logo.jpeg" />
+  //   </div>
+  // </div>
+  <Provider store={store()}>
+    <SkeletonTheme baseColor="#202020" highlightColor="#444">
+      <Router />
+    </SkeletonTheme>
+  </Provider>
 );
